8e76bdd666686798841af1cf03c1509794722ac1
Markus Beyer <markus.beyer10969@web.de>
2022-05-20 06:35:58 +0200

    DONE

--- simple_brute.py
+++ simple_brute.py
@@ -9,4 +9,19 @@ def Brute(pw):
    charset = list(string.ascii_letters + string.digits + string.punctuation + string.whitespace)
    result = ""
    x = 0
+   while x <= len(pw) -1:
+     for char in charset:
+       pwchar = pw[x]
+       if char == pwchar:
+         print(Fore.GREEN +"[+] Cracking ..." + Style.RESET_ALL , char)
+         print(Fore.LIGHTGREEN_EX + "[+][+]Matched (" + Style.RESET_ALL ,char, ")")
+         result += char
+         print("[+][+]Current:", result)
+         x += 1
+         break
+       else:
+         print(Fore.GREEN + "[+] Cracking ..." + Style.RESET_ALL,char)
+   print(Fore.YELLOW + "[+][+][+] Done. Password cracking finished:", result + Style.RESET_ALL)
+   finish = input("Press any key to end...")
    
+Brute(pw)
